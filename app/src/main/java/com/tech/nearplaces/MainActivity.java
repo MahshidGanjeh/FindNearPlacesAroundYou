83e0304e22fbfe11075ce08ea273948bd36d59db
Hp <mahshidga@gmail.com>
2019-07-29 23:26:31 +0430

    viewmodel almost completed

--- app/src/main/java/com/tech/nearplaces/MainActivity.java
+++ app/src/main/java/com/tech/nearplaces/MainActivity.java
@@ -1,48 +1,41 @@
 package com.tech.nearplaces;
 
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProviders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.tech.nearplaces.data.model.Item;
 import com.tech.nearplaces.data.model.Venue;
 import com.tech.nearplaces.data.remote.VenueRemoteDataSource;
 import com.tech.nearplaces.venues.VenueAdapter;
+import com.tech.nearplaces.venues.VenueItemViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    List<Item> resultItemsList = new ArrayList<>();
-    List<Venue> venueslist = new ArrayList<>();
-
+    VenueItemViewModel mVenueItemViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        VenueDataViewModel dataViewModel = new VenueDataViewModel();
-//        binding.setViewmodel(dataViewModel);
+        mVenueItemViewModel = ViewModelProviders.of(this).get(VenueItemViewModel.class);
+        ProgressBar progressBar = findViewById(R.id.progress_bar);
 
         RecyclerView recyclerView = findViewById(R.id.venue_rv);
         recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
 
-        VenueRemoteDataSource dataSource = new VenueRemoteDataSource();
-        dataSource.fetchVenues().subscribe(result ->
-        {
-            resultItemsList = result;
-            for (int i = 0; i < resultItemsList.size() - 1; i++) {
-                venueslist.add(resultItemsList.get(i).getVenue());
-            }
-            VenueAdapter adapter = new VenueAdapter(venueslist, getApplicationContext());
-            recyclerView.setAdapter(adapter);
-        });
-
-
-
+        setProgressBarIndeterminateVisibility(true);
+        mVenueItemViewModel.fetchVenuesFromRemote(recyclerView, getApplicationContext());
+        progressBar.setVisibility(View.GONE);
 
 
     }
--- app/src/main/java/com/tech/nearplaces/venues/VenueItemViewModel.java
+++ app/src/main/java/com/tech/nearplaces/venues/VenueItemViewModel.java
@@ -3,8 +3,20 @@ package com.tech.nearplaces.venues;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+
+import com.tech.nearplaces.data.model.Item;
+import com.tech.nearplaces.data.model.Venue;
+import com.tech.nearplaces.data.remote.VenueRemoteDataSource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class VenueItemViewModel extends ViewModel {
+
+    List<Item> resultItemsList = new ArrayList<>();
+    final List<Venue> venueslist = new ArrayList<>();
 
     MutableLiveData<String> _iconPath = new MutableLiveData<>();
     LiveData<String> iconPath = _iconPath;
@@ -13,4 +25,19 @@ public class VenueItemViewModel extends ViewModel {
     LiveData<String> venueName = _venueName;
 
     //MutableLiveData<String> _
+    public void fetchVenuesFromRemote(RecyclerView rv, Context context) {
+
+
+        VenueRemoteDataSource dataSource = new VenueRemoteDataSource();
+        dataSource.fetchVenues().subscribe(result ->
+        {
+            resultItemsList = result;
+            for (int i = 0; i < resultItemsList.size() - 1; i++) {
+                venueslist.add(resultItemsList.get(i).getVenue());
+            }
+            VenueAdapter adapter = new VenueAdapter(venueslist,context);
+            rv.setAdapter(adapter);
+        });
+
+    }
 }
